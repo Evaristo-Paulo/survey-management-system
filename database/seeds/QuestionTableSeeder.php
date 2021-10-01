64c6617ce4653624e693df79bcb5c4256fadebbe
Evaristo-Paulo <ivarilson909@gmail.com>
2021-10-01 02:59:30 +0100

    Alterando base de dados mysql para postgres

--- database/seeds/QuestionTableSeeder.php
+++ database/seeds/QuestionTableSeeder.php
@@ -13,33 +13,43 @@ class QuestionTableSeeder extends Seeder
      */
     public function run()
     {
-        Question::create([
+        $question = Question::create([
             'question' => 'Qual é a forma mais produtiva de trabalho?',
             'user_id' => 1,
             'vote' => 2,
-            'url' => URL::signedRoute('survey.vote.form', ['id' => encrypt(1)])
         ]);
-        Question::create([
+        $question->url = URL::signedRoute('survey.vote.form', ['id' => encrypt($question->id)]);
+        $question->save();
+
+        $question = Question::create([
             'question' => 'Qual é a tua linguagem de programação favorita?',
             'user_id' => 1,
-            'url' => URL::signedRoute('survey.vote.form', ['id' => encrypt(2)])
         ]);
-        Question::create([
+        $question->url = URL::signedRoute('survey.vote.form', ['id' => encrypt($question->id)]);
+        $question->save();
+
+        $question = Question::create([
             'question' => 'Quem é o melhor jogador de futebol do mundo?',
             'user_id' => 1,
             'vote' => 15,
-            'url' => URL::signedRoute('survey.vote.form', ['id' => encrypt(3)])
         ]);
-        Question::create([
+        $question->url = URL::signedRoute('survey.vote.form', ['id' => encrypt($question->id)]);
+        $question->save();
+
+        $question = Question::create([
             'question' => 'Estás satisfeito com a sua vida financeira?',
             'user_id' => 2,
             'url' => URL::signedRoute('survey.vote.form', ['id' => encrypt(4)])
         ]);
-        Question::create([
+        $question->url = URL::signedRoute('survey.vote.form', ['id' => encrypt($question->id)]);
+        $question->save();
+
+        $question = Question::create([
             'question' => 'Com qual frequência programas?',
             'user_id' => 2,
             'vote' => 2,
-            'url' => URL::signedRoute('survey.vote.form', ['id' => encrypt(5)])
         ]);
+        $question->url = URL::signedRoute('survey.vote.form', ['id' => encrypt($question->id)]);
+        $question->save();
     }
 }
